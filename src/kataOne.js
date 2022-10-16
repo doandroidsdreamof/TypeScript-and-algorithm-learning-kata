742eff74dde73ca171e2b5231388f0708c1070f4
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-16 21:27:12 +0300

    init

---
+++ src/kataOne.js
@@ -0,0 +1,8 @@
+"use strict";
+const test = (t) => console.log(t);
+let helloWorld = "Hello World";
+let value = 12;
+function sumAll(arr) {
+    return 1;
+}
+test(sumAll([1, 4]));
---
+++ src/kataOne.ts
@@ -0,0 +1,12 @@
+const test = (t:any) => console.log(t)
+
+let helloWorld : string = "Hello World";
+let value : number = 12;
+
+
+function sumAll(arr:number []) {
+    return 1;
+  }
+  
+test(sumAll([1, 4]))
+  
