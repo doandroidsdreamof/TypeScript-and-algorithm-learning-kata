085243b5b84bba0ef9243c14fcc5287fcf7c5741
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-17 19:50:40 +0300

    Spinal Tap Case Problem

--- src/kataOne.ts
+++ src/kataOne.ts
@@ -1,6 +1,7 @@
-const test = (t:any) => console.log(t)
-// Sum All Numbers in a Range
-function sumAll(arr: number[]) {
+const check = (t:any) => console.log(t)
+
+// Sum All Numbers in a Range //
+ function sumAll(arr: number[]) {
   let sum : number = 0;
   let maxValue: number  = Math.max(...arr)
   let minValue: number  = Math.min(...arr)
@@ -11,8 +12,33 @@ function sumAll(arr: number[]) {
   }
 
   return parseData.reduce((acc,el) => acc + el)
-
-
   }
   
-  test(sumAll([5, 10]))
+
+
+// Spinal Tap Case //
+
+  function spinalCase(str: string) {
+    let upperCase: RegExp = /[A-Z]/g; // for readability
+    let regexpUnderScore: RegExp = /[_-]+/g;
+    let regexpSpace: RegExp  = /\s/g;
+       if (
+       !str.match(regexpSpace) 
+       || str.match(regexpSpace) 
+       || str.match(regexpUnderScore)){
+       str = str.replace(upperCase, ' $&').trim().replace(regexpSpace,'-')
+      }
+      
+      return str.replace(regexpUnderScore,'-').toLowerCase()
+
+ 
+  }      
+
+  
+
+
+check(spinalCase('This Is Spinal Tap')) //this-is-spinal-tap
+check(spinalCase('thisIsSpinalTap')) //this-is-spinal-tap
+check(spinalCase('The_Andy_Griffith_Show')) //the-andy-griffith-show
+check(spinalCase('Teletubbies say Eh-oh')) // teletubbies-say-eh-oh
+check(spinalCase('AllThe-small Things')) // all-the-small-things
