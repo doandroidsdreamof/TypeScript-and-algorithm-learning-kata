453a153bf2aa4154d47c2afb027191db049c5e2d
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-10-30 17:13:51 +0300

    unused var

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -3,7 +3,6 @@
 // Formula f(n - 1) + f(n + 1 ) //
 function sumFibs(num: number) {
     let fibonacci: number[] = [];
-    let tekSayılarınToplamı = 0;
     let ilkDeğer = 0;
     let ikinciDeğer = 1;
     let n: number = num;
--- src/sumAllPrimes.ts
+++ src/sumAllPrimes.ts
@@ -1,9 +1,24 @@
-
 // Sum all prime numbers //
+// 6n + 1 //
 function sumPrimes(primeNumber: number) {
-    return primeNumber;
+  let arr: any[] = [];
+  let parse: any[] = [];
+  let n: number = primeNumber;
+  let singlePrimes: number[] = [2, 3, 5, 7];
+  let oddNumbers: number[] = [];
+  while (primeNumber ) {
+    primeNumber--;
+    arr.push(primeNumber);
   }
-  
-  console.log(sumPrimes(16))
-  
-  
+
+
+
+
+  return oddNumbers;
+
+  //.reduce((acc,el) => acc + el)
+}
+
+console.log(sumPrimes(10)); // 17
+console.log(sumPrimes(977)); // 73156
+debugger;
