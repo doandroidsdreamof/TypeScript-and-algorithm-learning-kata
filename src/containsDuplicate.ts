9bc64c60e9434fe53de74bb4b6e480d320f81cbb
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-01-25 18:34:30 +0300

    typo

--- src/containsDuplicate.ts
+++ src/containsDuplicate.ts
@@ -4,7 +4,6 @@ var containsDuplicate = function (nums: number[]) {
         [key: string]: number;
     };
     let hash: hashType = {};
-    let count = 0;
     let findDup = nums.forEach((current, index, arr) => {
         if (hash[current] == hash[current]) {
             hash[current] += 1;
@@ -24,8 +23,8 @@ var containsDuplicateTwo = function (nums: any[]) {
     return [...elSet].length == nums.length ? false : true;
 };
 
-console.log('false =>', containsDuplicateTwo([1, 2, 3, 4])); // false olmalı
-console.log('true =>', containsDuplicateTwo([1, 2, 3, 1])); // true olmalı
-console.log('true =>', containsDuplicateTwo([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true olmalı
+console.log('false =>', containsDuplicateTwo([1, 2, 3, 4])); // false 
+console.log('true =>', containsDuplicateTwo([1, 2, 3, 1])); // true
+console.log('true =>', containsDuplicateTwo([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
 console.log('true =>', containsDuplicateTwo([2, 14, 18, 22, 22])); //true
 debugger;
